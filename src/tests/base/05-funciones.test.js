98d1e5c626fee660910c5959fb0ebc7e1a542789
RaquelV <raquelv85@gmail.com>
2021-04-21 14:24:14 +0200

    add test to 05-funciones

---
+++ src/tests/base/05-funciones.test.js
@@ -0,0 +1,12 @@
+import { getUser } from '../../base/05-funciones';
+
+describe("Pruebas en 05-funciones", () => {
+  test("getUser debe de retornar un objeto", () => {
+    const userTest = {
+      uid: "ABC123",
+      username: "El_Papi1502",
+    };
+
+    
+  });
+});
