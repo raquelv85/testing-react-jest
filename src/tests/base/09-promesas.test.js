27974717021465600fb279675b6dba1c2b18abbf
RaquelV <raquelv85@gmail.com>
2021-04-22 11:39:44 +0200

    create file test 09-promesas

---
+++ src/tests/base/09-promesas.test.js
@@ -0,0 +1,13 @@
+import { getHeroeByIdAsync } from '../../base/09-promesas';
+
+
+describe('pruebas con promesas', () => {
+
+  test('getHeroeByIdAsync debe de retornar un Heroe async', () => {
+    const id = 1;
+
+      
+
+  });
+  
+})
