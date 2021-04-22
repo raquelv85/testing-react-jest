4f4358745915881cec03fb640ef05b68f185c920
RaquelV <raquelv85@gmail.com>
2021-04-22 11:15:26 +0200

    add test to check hero

--- src/tests/base/08-imp-exp.test.js
+++ src/tests/base/08-imp-exp.test.js
@@ -1,7 +1,18 @@
+import { getHeroeById } from '../../base/08-imp-exp';
+import heroes from '../../data/heroes';
+
 describe('pruebas en funciones de heroes', () => {
 
   test('debe de retornar un heroe por id', () => {
-    
-  })
+
+    const id = 1;
+
+    const heroe = getHeroeById(id);
+
+    const  heroeData = heroes.find(h => h.id === id);
+
+    expect( heroe ).toEqual( heroeData );
+
+  });
 
 })
