c2acc41f190f6d3d64884bf05ba26fd9e3cdc9f3
RaquelV <raquelv85@gmail.com>
2021-04-22 11:48:32 +0200

    add test to check if promise return a hero

--- src/tests/base/09-promesas.test.js
+++ src/tests/base/09-promesas.test.js
@@ -1,12 +1,18 @@
 import { getHeroeByIdAsync } from '../../base/09-promesas';
+import heroes from '../../data/heroes';
 
 
 describe('pruebas con promesas', () => {
 
-  test('getHeroeByIdAsync debe de retornar un Heroe async', () => {
+  // el argumento done se utiliza cuando hay asincronía, se esperará a que se ejecute, lo que hay dentro del then
+  test('getHeroeByIdAsync debe de retornar un Heroe async', ( done ) => {
     const id = 1;
 
-      
+    getHeroeByIdAsync( id )
+      .then( heroe => {
+        expect( heroe ).toBe( heroes[0] )
+        done(); // se pone para indicar que ya ha terminado el proceso
+      })
 
   });
   
