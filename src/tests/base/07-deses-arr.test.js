d5cbb5160d003fd04185b144a9524c2fae9f35bf
RaquelV <raquelv85@gmail.com>
2021-04-22 10:49:46 +0200

    add test and check width destructuring

--- src/tests/base/07-deses-arr.test.js
+++ src/tests/base/07-deses-arr.test.js
@@ -7,6 +7,15 @@ describe('Pruebas en 07-desestructuración',() => {
     const arr = retornaArreglo() //['ABC', 123]
 
     expect( arr ).toEqual(['ABC', 123]);
+  });
+
+  test('debe de retornar un string y un número',() => {
+    const [ letras, numeros ] = retornaArreglo() //['ABC', 123]
+
+    expect( letras ).toEqual('ABC');
+    expect( typeof letras ).toEqual('string');
+    expect( numeros ).toEqual( 123 );
+    expect( typeof numeros ).toEqual('number');
   })
 
 })
