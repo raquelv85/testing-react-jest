5cd0fbf02d40dd7adfc648bf92bdf54f4e64040f
RaquelV <raquelv85@gmail.com>
2021-04-23 12:26:55 +0200

    add test for check prop value

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -8,6 +8,13 @@ describe('Pruebas en <CounterApp />', () => {
     const wrapper = shallow( <CounterApp /> );
     expect( wrapper ).toMatchSnapshot();
 
+  });
+
+  test('debe de mostrar el valor por defecto de 100', () => {
+    const wrapper = shallow( <CounterApp value={ 100 }/> );
+    const counterText = wrapper.find('h2').text().trim();
+    
+    expect( counterText ).toBe('100');
   })
 
 })
