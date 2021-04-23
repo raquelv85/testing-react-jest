e34c6616e37974d00c3eade3b8d8369dc274b96e
RaquelV <raquelv85@gmail.com>
2021-04-23 12:42:10 +0200

    add test to simulate click -

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CounterApp from '../CounterApp';
+import React from "react";
+import { shallow } from "enzyme";
+import CounterApp from "../CounterApp";
 
-describe('Pruebas en <CounterApp />', () => {
+describe("Pruebas en <CounterApp />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);  //sirve pare resetear cada vez que se ejecuta un test
+  })
 
-  const wrapper = shallow( <CounterApp /> );
-
-  test('debe de mostrar <CounterApp /> correctamente', () => {
-    
-    expect( wrapper ).toMatchSnapshot();
-
+  test("debe de mostrar <CounterApp /> correctamente", () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
-  test('debe de mostrar el valor por defecto de 100', () => {
-    const wrapper = shallow( <CounterApp value={ 100 }/> );
-    const counterText = wrapper.find('h2').text().trim();
+  test("debe de mostrar el valor por defecto de 100", () => {
+    const wrapper = shallow(<CounterApp value={100} />);
+    const counterText = wrapper.find("h2").text().trim();
+
+    expect(counterText).toBe("100");
+  });
+
+  test("debe de incrementar con el botón +1", () => {
+    wrapper.find("button").at(0).simulate("click");
+
+    const counterText = wrapper.find("h2").text().trim();
+
+    expect(counterText).toBe("11");
+  });
+
+  test("debe de decrementar con el botón -1", () => {
     
-    expect( counterText ).toBe('100');
-  })
+    wrapper.find("button").at(2).simulate("click");
 
-  test('debe de incrementar con el botón +1', () => {
-   wrapper.find('button').at(0).simulate('click');
+    const counterText = wrapper.find("h2").text().trim();
 
-   const counterText = wrapper.find('h2').text().trim();
-
-   expect( counterText ).toBe('11')
-    
-  })
-
-})
+    expect(counterText).toBe("9");
+  });
+});
