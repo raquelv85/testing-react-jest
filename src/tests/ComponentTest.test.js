75ec0c6612df715be6ae08381a8475d4462098eb
RaquelV <raquelv85@gmail.com>
2021-04-23 11:36:53 +0200

    add test and check if component prop render ok

--- src/tests/ComponentTest.test.js
+++ src/tests/ComponentTest.test.js
@@ -18,8 +18,17 @@ describe('Pruebas en <ComponentTest/>',() => {
 
   test('debe de mostrar <ComponentTest/> correctamente', () => {
     const saludo = "Hola soy Raquel";
-    const wrapper = shallow(<ComponentTest/>);
+    const wrapper = shallow(<ComponentTest saludo={saludo}/>);
 
     expect( wrapper ).toMatchSnapshot();
+  });
+
+  test('debe de mostrar el subtitulo enviado por props', () => {
+    const saludo = "Hola soy Raquel";
+    const subtitulo = "subtitulo"
+    const wrapper = shallow(<ComponentTest saludo={saludo} subtitulo={subtitulo}/>);
+    const textoParrafo = wrapper.find('p').text();
+   
+    expect( textoParrafo ).toBe( subtitulo );
   })
 })
